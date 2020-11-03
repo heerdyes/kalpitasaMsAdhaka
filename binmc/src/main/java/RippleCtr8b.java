cbe7a9d3b65f09f7c1c040f98ef55c5de43a2d68
Heerdyes Mahapatro <heerdyes.mahapatro@gmail.com>
2020-11-03 18:22:03 +0530

    8 bit latch works

--- binmc/src/main/java/RippleCtr8b.java
+++ binmc/src/main/java/RippleCtr8b.java
@@ -1,28 +1,25 @@
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class RippleCtr8b extends TimerTask
-{
+public class RippleCtr8b extends TimerTask{
   long T;
   volatile boolean clk;
-  DFlipFlop f0, f1, f2, f3, f4, f5, f6, f7;
+  DFlipFlopOsc f0, f1, f2, f3, f4, f5, f6, f7;
 
-  public RippleCtr8b(long millis)
-  {
+  public RippleCtr8b(long millis){
     T = millis;
     clk = false;
-    f0 = new DFlipFlop();
-    f1 = new DFlipFlop();
-    f2 = new DFlipFlop();
-    f3 = new DFlipFlop();
-    f4 = new DFlipFlop();
-    f5 = new DFlipFlop();
-    f6 = new DFlipFlop();
-    f7 = new DFlipFlop();
+    f0 = new DFlipFlopOsc();
+    f1 = new DFlipFlopOsc();
+    f2 = new DFlipFlopOsc();
+    f3 = new DFlipFlopOsc();
+    f4 = new DFlipFlopOsc();
+    f5 = new DFlipFlopOsc();
+    f6 = new DFlipFlopOsc();
+    f7 = new DFlipFlopOsc();
   }
 
-  public void run()
-  {
+  public void run(){
     clk = !clk;
     f0.setClk(clk);
     f1.setClk(!f0.q);
@@ -32,8 +29,7 @@ public class RippleCtr8b extends TimerTask
     f5.setClk(!f4.q);
     f6.setClk(!f5.q);
     f7.setClk(!f6.q);
-    if(clk)
-    {
+    if(clk){
       System.out.println((f7.q?"1":"0") + (f6.q?"1":"0") + (f5.q?"1":"0") + (f4.q?"1":"0") + (f3.q?"1":"0") + (f2.q?"1":"0") + (f1.q?"1":"0") + (f0.q?"1":"0"));
     }
   }
@@ -42,11 +38,9 @@ public class RippleCtr8b extends TimerTask
   // --------- for testing ----------//
   /////////////////////////////////////
 
-  public static void main(String[] args)
-  {
+  public static void main(String[] args){
     RippleCtr8b c = new RippleCtr8b(100);
     Timer t = new Timer();
-
     t.scheduleAtFixedRate(c, c.T, c.T);
   }
 }
